4a0e0ff8078abec47030a7ec0934e86a3400123e
Max-shiri-90 <max.shiri.90@gmail.com>
2022-06-12 17:12:25 +0430

    users

--- automobile/settings.py
+++ automobile/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework_simplejwt',
+
     'core',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "core.User"
--- core/models.py
+++ core/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+from django.conf import settings
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError("Email must be provieded")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email,  password):
+
+        if not email:
+            raise ValueError("Email must be provieded")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        user.role = "admin"
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_CHOICES = (
+    ("reception","Reception"),
+    ("admin", "Admin"),
+    ("technician","Technician"),
+    ("inspector","Inspector"),
+    )
+
+    role = models.CharField(choices = USER_CHOICES, max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
